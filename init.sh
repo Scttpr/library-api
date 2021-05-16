200303038a4df3e92d5e116f917f9872bdbc5530
Scttpr <charles.capelli@qonfucius.team>
2021-05-16 20:18:53 +0200

    Added CI and init script

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+displayDone() {
+  echo 'done !'
+}
+
+echo 'Checking if NodeJS is installed'
+if which node > /dev/null
+  then
+    echo 'Node is correctly installed'
+  else
+    echo 'Installing NodeJS...'
+    apt install nodejs npm
+    displayDone
+  fi
+
+echo 'Moving to project directory...'
+cd data/library/api || exit
+displayDone
+
+echo 'Installing dependencies...'
+npm install
+displayDone
+
+echo 'Building project'
+npm run build
+displayDone
